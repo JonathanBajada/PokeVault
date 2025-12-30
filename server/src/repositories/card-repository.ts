e2e98a2977137df6cf64fcffda3245f62df51137
Jonatahn Bajada <jbajada@ryerson.ca>
2025-12-30 13:08:50 +0100

    added filtering for cards in repo and route

--- server/src/repositories/card-repository.ts
+++ server/src/repositories/card-repository.ts
@@ -1,20 +1,62 @@
 import { pool } from '../db';
 
-export async function getCards(page: number, limit: number) {
+type CardFilters = {
+	search?: string;
+	rarity?: string;
+	set?: string;
+};
+
+export async function getCards(
+	page: number,
+	limit: number,
+	filters: CardFilters,
+) {
 	const safeLimit = Math.min(limit, 20);
 	const offset = (page - 1) * safeLimit;
 
-	const dataPromise = pool.query(
-		`
+	const conditions: string[] = [];
+	const values: any[] = [];
+
+	// SEARCH (name)
+	if (filters.search) {
+		values.push(`%${filters.search}%`);
+		conditions.push(`name ILIKE $${values.length}`);
+	}
+
+	// RARITY
+	if (filters.rarity) {
+		values.push(filters.rarity);
+		conditions.push(`rarity = $${values.length}`);
+	}
+
+	// SET
+	if (filters.set) {
+		values.push(filters.set);
+		conditions.push(`set_name = $${values.length}`);
+	}
+
+	const whereClause =
+		conditions.length > 0 ? `WHERE ${conditions.join(' AND ')}` : '';
+
+	// Data query
+	const dataQuery = `
 		SELECT id, name, set_name, rarity, image_small
 		FROM cards
+		${whereClause}
 		ORDER BY name
-		LIMIT $1 OFFSET $2
-		`,
-		[safeLimit, offset],
-	);
+		LIMIT $${values.length + 1}
+		OFFSET $${values.length + 2}
+	`;
 
-	const countPromise = pool.query(`SELECT COUNT(*) FROM cards`);
+	// Count query
+	const countQuery = `
+		SELECT COUNT(*) FROM cards
+		${whereClause}
+	`;
+
+	const dataPromise = pool.query(dataQuery, [...values, safeLimit, offset]);
+
+	const countPromise = pool.query(countQuery, values);
 
 	const [dataResult, countResult] = await Promise.all([
 		dataPromise,
--- server/src/routes/cards.ts
+++ server/src/routes/cards.ts
@@ -9,7 +9,15 @@ router.get('/', async (req, res) => {
 	const page = Number(req.query.page ?? 1);
 	const limit = Number(req.query.limit ?? 10);
 
-	const { cards, total } = await getCards(page, limit);
+	const search = req.query.search?.toString();
+	const rarity = req.query.rarity?.toString();
+	const set = req.query.set?.toString();
+
+	const { cards, total } = await getCards(page, limit, {
+		search,
+		rarity,
+		set,
+	});
 
 	res.json({
 		page,
