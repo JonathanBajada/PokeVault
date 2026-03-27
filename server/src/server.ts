332153b42450f0757269a65f53c75c55ee9aba68
Jonatahn Bajada <jbajada@ryerson.ca>
2026-03-27 22:17:23 +0700

    updated hardcoded port

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ app.use('/users', usersRouter);
 console.log('Mounting /cards router');
 app.use('/cards', cardsRouter);
 
-const PORT = 4000;
+const PORT = process.env.PORT ?? 4000;
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
